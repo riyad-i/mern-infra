822a4a92e36d8eb9be313621b256f9909151d9ed
Riyad <Riyad9876@gmail.com>
2023-09-11 16:19:59 -0400

    controlled form functionality

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -9,25 +9,41 @@ function SignUpForm(){
         error: ''
     })
 
+    const disable = formData.password !== formData.confirm
+
+    const handleChange = (e)=>{
+        console.log(e);
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value,
+            error: ''
+        })
+    }
+
+    const handleSubmit = ()=>{
+
+    }
+
     return(
         <div>
-            <div>
-                <form>
+            <div className="form-container">
+                <form autoComplete="off" onSubmit={handleSubmit}>
                     <label>Name</label>
-                    <input type="text" name="name" required/>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} required/>
                     
                     <label>Email</label>
-                    <input type="email" name="email" required/>
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} required/>
 
                     <label>Password</label>
-                    <input type="password" name="password" required/>
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} required/>
 
                     <label>Confirm Password</label>
-                    <input type="password" name="confirm" required/>
+                    <input type="password" name="confirm" value={formData.confirm} onChange={handleChange} required/>
 
-                    <button>Sign Up</button>
+                    <button disabled={disable}>Sign Up</button>
                 </form>
             </div>
+            <p className="error-message">{formData.error}</p>
         </div>
     )
 }
