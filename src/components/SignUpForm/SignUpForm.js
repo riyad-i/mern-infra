14a9cdbb954d7f5a610cf0be0849fca0a526ecba
Riyad <Riyad9876@gmail.com>
2023-09-11 15:59:25 -0400

    basic signupform

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,34 @@
+import { useState } from "react"
+
 function SignUpForm(){
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
+    })
+
     return(
-        <h1>SignUpForm</h1>
+        <div>
+            <div>
+                <form>
+                    <label>Name</label>
+                    <input type="text" name="name" required/>
+                    
+                    <label>Email</label>
+                    <input type="email" name="email" required/>
+
+                    <label>Password</label>
+                    <input type="password" name="password" required/>
+
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm" required/>
+
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
