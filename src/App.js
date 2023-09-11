bbe00250fb82e31e5990b04a46da2f6e1104c805
Riyad <Riyad9876@gmail.com>
2023-09-11 14:13:37 -0400

    routes in app.js, orders/new and orders rendering NewOrderPage and OrderHistoryPage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { useState } from "react";
 import NewOrderPage from "./pages/NewOrderPage";
 import AuthPage from "./pages/AuthPage";
 import OrderHistoryPage from "./pages/OrderHistoryPage";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
     <div className="App">
-      {user ? <NewOrderPage /> : <AuthPage />}
+      {user ? (
+        <Routes>
+          <Route path='orders/new' element={<NewOrderPage/>}/>
+          <Route path='orders' element={<OrderHistoryPage/>}/>
+        </Routes>
+      ) : <AuthPage />}
     </div>
   )
 }
