8f3b57dce02aa570ada062b0d703856ca0f639fe
Riyad <Riyad9876@gmail.com>
2023-09-11 14:25:50 -0400

    src/components/NavBar.js, rendering in App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import NewOrderPage from "./pages/NewOrderPage";
 import AuthPage from "./pages/AuthPage";
 import OrderHistoryPage from "./pages/OrderHistoryPage";
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
 
   return (
     <div className="App">
       {user ? (
+        <>
+        <NavBar/>
         <Routes>
           <Route path='orders/new' element={<NewOrderPage/>}/>
           <Route path='orders' element={<OrderHistoryPage/>}/>
         </Routes>
+        </>
       ) : <AuthPage />}
     </div>
   )
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+
+function NavBar(){
+    return(
+        <nav>
+            <h3>navbar</h3>
+        </nav>
+    )
+}
+
+
+export default NavBar
