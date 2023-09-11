9223fdcc5c8a96e71af42720d3d6b5c509f7d8a1
Riyad <Riyad9876@gmail.com>
2023-09-11 12:49:49 -0400

    ternary in app.js to render specific page based on user login state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import { useState } from 'react';
-
-
+import "./App.css";
+import { useState } from "react";
+import NewOrderPage from "./pages/NewOrderPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <div className="App">
-      
-        
+      {user ? <NewOrderPage /> : <AuthPage />}
     </div>
-  );
+  )
 }
 
 export default App;
