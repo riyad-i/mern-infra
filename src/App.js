707e5bd6b4e950fe600dcf3b62a807a905434513
Riyad <Riyad9876@gmail.com>
2023-09-11 14:07:58 -0400

    browser router index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import NewOrderPage from "./pages/NewOrderPage";
 import AuthPage from "./pages/AuthPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage";
 
 function App() {
   const [user, setUser] = useState(null);
