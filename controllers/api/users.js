20d5d48eca9d639b44df35b4b94c681832f794c3
Riyad <Riyad9876@gmail.com>
2023-09-12 15:16:24 -0400

    createJWT helper function

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,25 +1,28 @@
 const User = require('../../models/user')
 
+const jwt = require('jsonwebtoken')
+
+
 async function create(req,res){
     try {
         const user = await User.create(req.body)
-        console.log(user);
-        res.json(user)
+        const token = createJWT(user)
+        res.json(token)
     } catch (error) {
         res.status(400).json(error)
     }
-
-
-
-    // res.json({
-    //     user: {
-    //         name: req.body.name,
-    //         email: req.body.email
-    //     }
-    // })
 }
 
 
+function createJWT(user){
+    return(jwt.sign(
+        //data payload
+        {user},
+        process.env.SECRET,
+        {expiresIn: '24h'}
+
+    ))
+}
 
 
 
