002a8063456b1b1972f9c382293d98e3a98d346c
Riyad <Riyad9876@gmail.com>
2023-09-12 11:17:45 -0400

    post fetch in users-api.js

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,10 +2,11 @@ import * as usersAPI from "./users-api";
 
 
 export async function signUp(userData){
-    console.log('useer data', userData);
-    await usersAPI.signUp(userData)
+    console.log('user data', userData);
+    const token = await usersAPI.signUp(userData)
+    return token
     // Delegate the network request code to the users-api.js API module
-  // which will ultimately return a JSON Web Token (JWT)
+    // which will ultimately return a JSON Web Token (JWT)
 
     //Note: We have not used a try/catch block because any error will propagate up to the "consumer" of the service - in this case the consumer is the handleSubmit method in the <SignUpForm> component.
 }
