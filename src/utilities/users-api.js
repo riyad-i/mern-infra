312abd71ae43561621787837774a95157ee8c7b1
Riyad <Riyad9876@gmail.com>
2023-09-14 14:43:37 -0400

    token and authorization in sendRequest in users-api

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,3 +1,5 @@
+import { getToken } from "./users-service";
+
 const BASE_URL = '/api/users';
 
 export function signUp(userData) {
@@ -23,6 +25,16 @@ async function sendRequest(url, method = 'GET', payload = null) {
     options.headers = { 'Content-Type': 'application/json' };
     options.body = JSON.stringify(payload);
   }
+
+  const token = getToken();
+  if (token) {
+    // Ensure the headers object exists
+    options.headers = options.headers || {};
+    // Add token to an Authorization header
+    // Prefacing with 'Bearer' is recommended in the HTTP specification
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+
   const res = await fetch(url, options);
   // res.ok will be false if the status code set to 4xx in the controller action
   if (res.ok) return res.json();
