a116bd3078371be625a09158b83dd34b6d24de73
Riyad <Riyad9876@gmail.com>
2023-09-14 12:14:03 -0400

    login controller finding user email in db, matching passwords and sending token

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -2,6 +2,7 @@ const User = require('../../models/user')
 
 const jwt = require('jsonwebtoken')
 
+const bcrypt = require('bcrypt')
 
 async function create(req,res){
     try {
@@ -30,6 +31,34 @@ async function login(req, res){
     //find user by email
     //compare password with hashed password
     //create token
+    try {
+        //find user by email
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) throw new Error();
+        //compare password
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) throw new Error();
+        res.json( createJWT(user) );
+      } catch {
+        res.status(400).json('Bad Credentials');
+      }
+
+
+    // try {
+    //     const user = User.findOne({email: req.body.email})
+    //     if (user) {
+    //         console.log('found user:', user);
+    //         const match = bcrypt.compare(req.body.password, user.password)
+    //         if (match){
+    //             const token = createJWT(user)
+    //             res.json(token)
+    //         }
+    //     }
+
+    // } catch (error) {
+    //     console.log(error);
+    //     res.status(400).json(error)
+    // }
 }
 
 
