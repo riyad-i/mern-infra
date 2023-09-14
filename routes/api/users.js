3b04622e3dfb19d11f149cceb4f0caee3b19289c
Riyad <Riyad9876@gmail.com>
2023-09-14 15:53:02 -0400

    ensureLoggedIn to protect routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,13 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 const usersCtrl = require('../../controllers/api/users')
-
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 //POST
 router.post('/', usersCtrl.create)
 
 router.post('/login', usersCtrl.login)
 
-router.get('/check-token', usersCtrl.checkToken)
+
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn , usersCtrl.checkToken)
 
 module.exports = router
